434474f05ba71c43aabd2b04158fd1037066b11a
dell <beau.wilkinson@gmail.com>
2019-01-15 20:46:22 -0500

    Added licensing terms

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject porpus-proj/lein-template "1.0.0"
   :description "Porpus: A Leiningen Template for Web Development"
   :url "https://github.com/beau1399/porpus"
+  :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}
   :eval-in-leiningen true)
--- resources/leiningen/new/porpus_proj/project.clj
+++ resources/leiningen/new/porpus_proj/project.clj
@@ -1,9 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-
+  :license {:name "Unlicense" :url "http://unlicense.org/UNLICENSE"}  
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring-server "0.5.0"]
                  [ring "1.7.1"]
